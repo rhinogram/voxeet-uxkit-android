b0eecd200d126fddb679ca7c09f43e6b4682cc28
Mary Hart <mary@rhinogram.com>
2020-05-01 21:01:39 -0400

    cleans up

--- uxkit/src/main/java/com/voxeet/uxkit/implementation/VoxeetActionBarView.java
+++ uxkit/src/main/java/com/voxeet/uxkit/implementation/VoxeetActionBarView.java
@@ -435,42 +435,10 @@ public class VoxeetActionBarView extends VoxeetView {
     }
 
     /**
-     * Toggle the mute button
+     * Toggle the flip button
      */
 
      public void toggleFlip() {
-         // String ownUserId = VoxeetSDK.session().getParticipantId();
-         // if (null == ownUserId) ownUserId = "";
-         //
-         // VideoView self = selfVideoView.get();
-         // VideoView other = otherVideoView.get();
-         //
-         // VideoView finalVideoView = null;
-         //
-         // if (null != self && ownUserId.equals(self.getPeerId())) {
-         //     finalVideoView = self;
-         // } else if (null != other && ownUserId.equals(other.getPeerId())) {
-         //     finalVideoView = other;
-         // }
-
-         //if (null != finalVideoView && finalVideoView == self) { //force for now the effect only in the SELF VIDEO VIEW
-             //until a proper animation is found
-
-             //switchCamera should not trigger crash since it is only possible
-             //to click when already capturing and ... rendering the camera
-
-             //ObjectAnimator animationFlip = ObjectAnimator.ofFloat(finalVideoView, View.ROTATION_Y, -180f, -360f);
-            // animationFlip.setInterpolator(new AccelerateDecelerateInterpolator());
-
-             //ObjectAnimator animationGrow = ObjectAnimator.ofFloat(finalVideoView, View.SCALE_Y, 1f, 1.15f, 1f);
-             //animationGrow.setInterpolator(new AccelerateDecelerateInterpolator());
-             //
-             // AnimatorSet animatorSet = new AnimatorSet();
-             // animatorSet.setDuration(450).setStartDelay(450);
-             // animatorSet.playTogether(animationFlip, animationGrow);
-             // animatorSet.start();
-         //}
-
          VoxeetSDK.mediaDevice().switchCamera()
                  .then(aBoolean -> {
                      CameraContext provider = VoxeetSDK.mediaDevice().getCameraContext();
@@ -478,42 +446,7 @@ public class VoxeetActionBarView extends VoxeetView {
                  })
                  .error(Throwable::printStackTrace);
      }
-     // public ConferenceViewRendererControl(@NonNull VoxeetConferenceView parent,
-     //                                      @NonNull VideoView selfVideoView,
-     //                                      @NonNull VideoView otherVideoView) {
-     //     this();
-     //     this.parent = new WeakReference<>(parent);
-     //     this.selfVideoView = new WeakReference<>(selfVideoView);
-     //     this.otherVideoView = new WeakReference<>(otherVideoView);
-     // }
 
-     // @NonNull
-     // private VideoView getOtherVideoView() {
-     //     return otherVideoView.get();
-     // }
-     //
-     // @NonNull
-     // private VideoView getSelfVideoView() {
-     //     return selfVideoView.get();
-     // }
-     //
-     // public void updateMirror(boolean isFrontCamera) {
-     //     String ownUserId = VoxeetSDK.session().getParticipantId();
-     //     VideoView selectedView = getOtherVideoView();
-     //     VideoView selfView = getSelfVideoView();
-     //
-     //     if (null != ownUserId) {
-     //         if (null != selectedView && ownUserId.equals(selectedView.getPeerId())) {
-     //             //only mirror the view in case of camera stream
-     //             MediaStreamType type = selectedView.current();
-     //             if (MediaStreamType.Camera.equals(type)) {
-     //                 selectedView.setMirror(isFrontCamera);
-     //             }
-     //         } else if (null != selfView && ownUserId.equals(selfView.getPeerId())) {
-     //             selfView.setMirror(isFrontCamera);
-     //         }
-     //     }
-     // }
     /**
      * Activate or deactivate the local camera
      */
